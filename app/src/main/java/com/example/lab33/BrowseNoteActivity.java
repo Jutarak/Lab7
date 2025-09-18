b98a41ea76c135a7927294ca03454d34cc73d684
ws-05 <nontawat.y@psru.ac.th>
2025-09-18 15:36:37 +0700

    Lad8

--- app/src/main/java/com/example/lab33/BrowseNoteActivity.java
+++ app/src/main/java/com/example/lab33/BrowseNoteActivity.java
@@ -17,12 +17,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class BrowseNoteActivity extends AppCompatActivity {
 
     Button addsearch,back2;
     ProgressBar progressBar2;
     TextView display2;
     EditText editTextText5;
+    TextView showNote , showNoteFromAPI;
 
 
 
@@ -44,6 +55,60 @@ public class BrowseNoteActivity extends AppCompatActivity {
                 System.out.println("Click!!");
                 Intent addBack = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(addBack);
+            }
+        });
+
+        showNote = findViewById(R.id.textView6);
+        showNoteFromAPI = findViewById(R.id.textView7);
+
+        //context.context=viwe.getContext();
+        //load data from db
+        Executors.newSingleThreadExecutor().execute(()->{
+            List<NoteEntity>entities = AppDatabase.getInstance(getApplicationContext()).noteDao().getAll();
+            List<Note>notes = new ArrayList<>();
+            for (NoteEntity e : entities){
+                notes.add(NoteMapper.fromEntity(e));
+            }
+
+            //display on UI thread
+            runOnUiThread(()  -> {
+                StringBuilder sb = new StringBuilder();
+                for (Note n : notes){
+                    sb.append(n.getSummary()).append("\n");
+                }
+                showNote.setText(sb.toString());
+            });
+        });
+
+        //load from API
+        Retrofit retrofit  = new Retrofit.Builder()
+                .baseUrl("https://jsonpiaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        Call<List<TextNote>>call = apiService.getTextNote();
+
+        call.enqueue(new Callback<List<TextNote>>() {
+            @Override
+            public void onResponse(Call<List<TextNote>> call, Response<List<TextNote>> response) {
+                if (!response.isSuccessful()){
+                    showNoteFromAPI.setText("Error Code:"+response.code());
+                    return;
+                }
+                List<TextNote>notes = response.body();
+                StringBuilder builder = new StringBuilder();
+                for (TextNote n : notes){
+                    builder.append("Title: ").append(n.getTitle()).append("\n")
+                            .append("Body: ").append(n.getTextContent()).append("\n\n");
+                }
+                showNoteFromAPI.setText(builder.toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<TextNote>> call, Throwable t) {
+                showNoteFromAPI.setText("Failad: "+t.getMessage() );
+
             }
         });
 
